e35c77e7caab39cea84e00a91ce5bbde9527e818
Unknown <smirnov.v22.04.1996@gmail.com>
2018-08-16 07:45:36 +0600

    #5 Shell-script

---
+++ src/main/bin/findmax.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+PRG="$0"
+BASEDIR=`dirname "$PRG"`
+if [ -z "$JAVA_HOME" ] ; then
+  if [ -r /etc/gentoo-release ] ; then
+    JAVA_HOME=`java-config --jre-home`
+  fi
+fi
+
+# If a specific java binary isn't specified search for the standard 'java' binary
+if [ -z "$JAVACMD" ] ; then
+  if [ -n "$JAVA_HOME"  ] ; then
+    if [ -x "$JAVA_HOME/jre/sh/java" ] ; then
+      # IBM's JDK on AIX uses strange locations for the executables
+      JAVACMD="$JAVA_HOME/jre/sh/java"
+    else
+      JAVACMD="$JAVA_HOME/bin/java"
+    fi
+  else
+    JAVACMD=`which java`
+  fi
+fi
+
+if [ ! -x "$JAVACMD" ] ; then
+  echo "Error: JAVA_HOME is not defined correctly." 1>&2
+  echo "  We cannot execute $JAVACMD" 1>&2
+  exit 1
+fi
+
+exec "$JAVACMD" $JAVA_OPTS -jar $BASEDIR/${artifactId}-${version}.jar "$@"
